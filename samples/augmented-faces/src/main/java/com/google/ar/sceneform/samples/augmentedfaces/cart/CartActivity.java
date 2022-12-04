46667f0990602327c21e6dd66d89b833f955b388
Paul J <jorpaulvincent@gmail.com>
2022-12-04 15:45:48 +0800

    with cart

---
+++ samples/augmented-faces/src/main/java/com/google/ar/sceneform/samples/augmentedfaces/cart/CartActivity.java
@@ -0,0 +1,278 @@
+package com.google.ar.sceneform.samples.augmentedfaces.cart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.sceneform.samples.augmentedfaces.R;
+import com.google.ar.sceneform.samples.augmentedfaces.form.FormOrder;
+import com.google.ar.sceneform.samples.augmentedfaces.product.Product;
+import com.google.ar.sceneform.samples.augmentedfaces.product.service.ProductList;
+import com.google.ar.sceneform.samples.augmentedfaces.product_catalog.ProductCatalog;
+import com.nex3z.notificationbadge.NotificationBadge;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CartActivity extends AppCompatActivity {
+
+    public static NotificationBadge cart_badge;
+    public static Button checkOut;
+    public static List<Map<String, Object>> mySimpleList;
+
+    private ProductList prod_list = new ProductList();
+    private ListView item_list;
+    private ImageButton buttonBack, cartButton;
+    private NotificationBadge cartBadge;
+    Button shop;
+    private TextView message, subTotal;
+    private RelativeLayout layout;
+    private ImageView title_bar;
+
+    private DecimalFormat df = new DecimalFormat(",###.00");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        //hide on-screen bottom navigation
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //list of items
+        item_list = (ListView) findViewById(R.id.cart_list);
+        //your cart is empty
+        message = (TextView) findViewById(R.id.cart_message);
+        //go shopping now
+        shop = (Button) findViewById(R.id.cart_button_shop);
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CartActivity.this, ProductCatalog.class);
+                startActivity(intent);
+            }
+        });
+
+        //cart badge
+        cart_badge = (NotificationBadge) findViewById(R.id.catalog_cart_badge);
+        if (prod_list.counter == 0){
+            message.setVisibility(View.VISIBLE);
+            shop.setVisibility(View.VISIBLE);
+            item_list.setVisibility(View.INVISIBLE);
+        }else{
+            message.setVisibility(View.INVISIBLE);
+            shop.setVisibility(View.INVISIBLE);
+            item_list.setVisibility(View.VISIBLE);
+            cart_badge.setNumber(prod_list.counter);
+        }
+
+        //Adapter 1
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, prod_list.getItem());
+
+        mySimpleList = new ArrayList<>();
+        prod_list.createList();
+        String [] from = {"photo","name", "price", "quantity", "total" };
+        int [] to = {R.id.list_itemPhoto, R.id.list_itemName, R.id.list_itemPrice, R.id.list_itemQuantity, R.id.list_itemTotal };
+        //Adapter2
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), mySimpleList, R.layout.listview_design, from, to);
+
+        //set item list
+        item_list.setAdapter(simpleAdapter);
+
+        title_bar = (ImageView) findViewById(R.id.app_bar_title);
+        title_bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CartActivity.this, ProductCatalog.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonBack = (ImageButton) findViewById(R.id.button_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = intent = new Intent(CartActivity.this, ProductCatalog.class);
+                startActivity(intent);
+            }
+        });
+
+        item_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int item_position = 0;
+                for (Product myList : prod_list.list) {
+                    if (position == item_position){
+
+                        if (myList.name.equals("MAGIC PINK LIP GLOSS")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("BLENDABLE LIP AND CHEEK COLOR")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("BLENDABLE LIP AND CHEEK COLOR ANGEL")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("METALLIC PRO LIPSTICK")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("MATTE PRO LIPSTICK")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("MATTE PRO LIPSTICK BLITZ")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("LIP GLOSS")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("LIP CHEEK EYE COLOR")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("LONG LASTING METALLIC LIP COLOR")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("LONG LASTING LIP COLOR")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("CLASSIQUE LIPSTICK")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("METALLIC PRO LIPSTICK HOLLYWOOD")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }else if (myList.name.equals("MAGIC PINK LIP GLOSS - 5 PCS BUNDLE")){
+                            Intent intent = new Intent(CartActivity.this, CartItemActivity.class);
+                            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+                            intent.putExtra("NAME", myList.name);
+                            intent.putExtra("PRICE", myList.price);
+                            intent.putExtra("QUANTITY", myList.quantity);
+                            intent.putExtra("TOTAL", myList.total);
+                            startActivity(intent);
+
+                        }
+
+//                        Toast.makeText(CartActivity.this, myList.name,Toast.LENGTH_SHORT).show();
+                        item_position = 0;
+                        break;
+                    }
+                    item_position++;
+                }
+            }
+        });
+
+        checkOut = (Button) findViewById(R.id.cart_CheckOut);
+        checkOut.setText("Check Out ("+prod_list.counter+")");
+
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prod_list.checkOut();
+                Intent intent = new Intent(CartActivity.this, FormOrder.class);
+                startActivity(intent);
+            }
+        });
+
+        subTotal = (TextView) findViewById(R.id.cart_subTotal);
+        subTotal.setText("₱"+df.format(prod_list.getSubTotal()));
+
+        layout = (RelativeLayout) findViewById(R.id.bottom_layout);
+        if (prod_list.getSubTotal()==0){
+            layout.setVisibility(View.INVISIBLE);
+        }else{
+            layout.setVisibility(View.VISIBLE);
+        }
+
+        cartButton = (ImageButton) findViewById(R.id.catalog_cart_icon);
+        cartButton.setVisibility(View.INVISIBLE);
+
+        cartBadge = (NotificationBadge) findViewById(R.id.catalog_cart_badge);
+        cartBadge.setVisibility(View.INVISIBLE);
+    }
+}
---
+++ samples/augmented-faces/src/main/java/com/google/ar/sceneform/samples/augmentedfaces/product_view/ProductView.java
@@ -0,0 +1,280 @@
+package com.google.ar.sceneform.samples.augmentedfaces.product_view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.sceneform.samples.augmentedfaces.R;
+import com.google.ar.sceneform.samples.augmentedfaces.cart.CartActivity;
+import com.google.ar.sceneform.samples.augmentedfaces.product.service.ProductList;
+import com.google.ar.sceneform.samples.augmentedfaces.product_catalog.ProductCatalog;
+import com.google.ar.sceneform.samples.augmentedfaces.product_details.ProductDetails;
+import com.nex3z.notificationbadge.NotificationBadge;
+import com.google.ar.sceneform.samples.augmentedfaces.MainActivity;
+
+public class ProductView extends AppCompatActivity {
+    public static NotificationBadge prod_view_badge;
+    private ProductList prod_list = new ProductList();
+    private ImageButton buttonBack, cartIcon;
+    private ImageView title_bar;
+    //Bundle
+    private String product_label, product_price, product_weight, product_details;
+    private int product_image;
+    //
+    private Intent intent = new Intent();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_view);
+
+        //hide on-screen bottom navigation
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        ImageView imageView = (ImageView) findViewById(R.id.viewProductImage);
+        TextView item_name = (TextView) findViewById(R.id.viewProductName);
+        TextView item_price = (TextView) findViewById(R.id.viewProductPrice);
+        TextView item_weight = (TextView) findViewById(R.id.viewProductWeight);
+        TextView item_details = (TextView) findViewById(R.id.viewProductDetails);
+
+        //Get Value From Catalog
+        Bundle bundle = getIntent().getExtras();
+        if (bundle!=null){
+            product_label = bundle.getString("LABEL");
+            product_price = bundle.getString("PRICE");
+            product_image = bundle.getInt("IMAGE");
+            product_weight = bundle.getString("WEIGHT");
+            product_details = bundle.getString("DETAILS");
+
+            item_name.setText(product_label);
+            item_price.setText(product_price);
+            imageView.setImageResource(product_image);
+            item_weight.setText(product_weight);
+            item_details.setText(product_details);
+        }
+
+        title_bar = (ImageView) findViewById(R.id.app_bar_title);
+        title_bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ProductView.this, ProductCatalog.class);
+                startActivity(intent);
+            }
+        });
+
+        cartIcon = (ImageButton) findViewById(R.id.catalog_cart_icon);
+        cartIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ProductView.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        prod_view_badge = (NotificationBadge) findViewById(R.id.catalog_cart_badge);
+        prod_view_badge.setNumber(prod_list.counter);
+
+        buttonBack = (ImageButton) findViewById(R.id.button_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void Buy(View v) {
+
+        if (product_label.equals("MAGIC PINK LIP GLOSS")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "MAGIC PINK LIP GLOSS");
+            intent.putExtra("PRICE", "75.90");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("BLENDABLE LIP AND CHEEK COLOR")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "BLENDABLE LIP AND CHEEK COLOR");
+            intent.putExtra("PRICE", "81.40");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("BLENDABLE LIP AND CHEEK COLOR ANGEL")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "BLENDABLE LIP AND CHEEK COLOR ANGEL");
+            intent.putExtra("PRICE", "81.40");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("METALLIC PRO LIPSTICK")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "METALLIC PRO LIPSTICK");
+            intent.putExtra("PRICE", "86.80");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("MATTE PRO LIPSTICK")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "MATTE PRO LIPSTICK");
+            intent.putExtra("PRICE", "86.80");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("MATTE PRO LIPSTICK BLITZ")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "MATTE PRO LIPSTICK BLITZ");
+            intent.putExtra("PRICE", "86.80");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("LIP GLOSS")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "LIP GLOSS");
+            intent.putExtra("PRICE", "97.70");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("LIP CHEEK EYE COLOR")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "LIP CHEEK EYE COLOR");
+            intent.putExtra("PRICE", "108.70");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("LONG LASTING METALLIC LIP COLOR")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "LONG LASTING METALLIC LIP COLOR");
+            intent.putExtra("PRICE", "119.60");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("LONG LASTING LIP COLOR")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "LONG LASTING LIP COLOR");
+            intent.putExtra("PRICE", "119.60");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("CLASSIQUE LIPSTICK")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "CLASSIQUE LIPSTICK");
+            intent.putExtra("PRICE", "119.60");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("METALLIC PRO LIPSTICK HOLLYWOOD")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "METALLIC PRO LIPSTICK HOLLYWOOD");
+            intent.putExtra("PRICE", "86.80");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }else if (product_label.equals("MAGIC PINK LIP GLOSS - 5 PCS BUNDLE")){
+            intent = new Intent(this, ProductDetails.class);
+            intent.putExtra("LABEL", "MAGIC PINK LIP GLOSS - 5 PCS BUNDLE");
+            intent.putExtra("PRICE", "297.60");
+            intent.putExtra("PICTURE", R.drawable.blendable_lip_angel);
+            startActivity(intent);
+
+        }
+
+    }
+
+    public void Try(View v){
+
+        if (product_label.equals("MAGIC PINK LIP GLOSS")){
+            intent = new Intent(this, MainActivity.class);
+            intent.putExtra("LABEL", "MAGIC PINK LIP GLOSS");
+            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+            startActivity(intent);
+
+        }
+
+//        if (product_label.equals("MAGIC PINK LIP GLOSS")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "MAGIC PINK LIP GLOSS");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("BLENDABLE LIP AND CHEEK COLOR")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "BLENDABLE LIP AND CHEEK COLOR");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("BLENDABLE LIP AND CHEEK COLOR ANGEL")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "BLENDABLE LIP AND CHEEK COLOR ANGEL");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("METALLIC PRO LIPSTICK")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "METALLIC PRO LIPSTICK");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("MATTE PRO LIPSTICK")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "MATTE PRO LIPSTICK");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("MATTE PRO LIPSTICK BLITZ")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "MATTE PRO LIPSTICK BLITZ");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("LIP GLOSS")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "LIP GLOSS");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("LIP CHEEK EYE COLOR")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "LIP CHEEK EYE COLOR");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("LONG LASTING METALLIC LIP COLOR")) {
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "LONG LASTING METALLIC LIP COLOR");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("LONG LASTING LIP COLOR")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "LONG LASTING LIP COLOR");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("CLASSIQUE LIPSTICK")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "CLASSIQUE LIPSTICK");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("METALLIC PRO LIPSTICK HOLLYWOOD")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "METALLIC PRO LIPSTICK HOLLYWOOD");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }else if (product_label.equals("MAGIC PINK LIP GLOSS - 5 PCS BUNDLE")){
+//            intent = new Intent(this, MainActivity.class);
+//            intent.putExtra("LABEL", "MAGIC PINK LIP GLOSS - 5 PCS BUNDLE");
+//            intent.putExtra("MAKEUP", R.drawable.makeup_5);
+//            startActivity(intent);
+//
+//        }
+    }
+}
