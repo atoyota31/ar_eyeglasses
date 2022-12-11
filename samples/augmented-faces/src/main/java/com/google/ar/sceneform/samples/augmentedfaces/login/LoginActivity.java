dbf85eacf7eea7f4c03b14315df9cefecb143098
Paul J <jorpaulvincent@gmail.com>
2022-12-11 15:13:06 +0800

    create login

---
+++ samples/augmented-faces/src/main/java/com/google/ar/sceneform/samples/augmentedfaces/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.google.ar.sceneform.samples.augmentedfaces.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.ar.sceneform.samples.augmentedfaces.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
